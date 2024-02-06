04e8abae4ad5ecaabb6e4c64c6d7ad0824a01f8e
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2024-02-06 12:34:31 +0100

    init

---
+++ 10-w3i-gm-dapp/src/pages/_app.tsx
@@ -0,0 +1,59 @@
+import "@/styles/globals.css";
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import {
+	arbitrum,
+	avalanche,
+	bsc,
+	fantom,
+	gnosis,
+	mainnet,
+	optimism,
+	polygon,
+} from "wagmi/chains";
+
+const chains = [
+	mainnet,
+	polygon,
+	avalanche,
+	arbitrum,
+	bsc,
+	optimism,
+	gnosis,
+	fantom,
+];
+
+// 1. Get projectID at https://cloud.walletconnect.com
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "";
+
+const metadata = {
+	name: "Next Starter Template",
+	description: "A Next.js starter template with Web3Modal v3 + Wagmi",
+	url: "https://web3modal.com",
+	icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+export default function App({ Component, pageProps }: AppProps) {
+	const [ready, setReady] = useState(false);
+
+	useEffect(() => {
+		setReady(true);
+	}, []);
+	return (
+		<>
+			{ready ? (
+				<WagmiConfig config={wagmiConfig}>
+					<Component {...pageProps} />
+				</WagmiConfig>
+			) : null}
+		</>
+	);
+}
